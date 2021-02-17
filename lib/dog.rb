cfb3fa6cfc928a3847083dc0583f5ee45900d937
Jason Whitlock <jrwhitlock7@gmail.com>
2021-02-17 12:34:02 -0700

    commit tree 1a2be8614a39e5f20a61b3aa3a2e9a4d10bba723

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+    attr_accessor :name, :breed
+
+    def initialize
+        @name = name
+        @breed = breed
+    end
+
+    # def name=(name)
+    #     @name = name
+    # end
+
+    # def name
+    #     @name
+    # end
+
+end
